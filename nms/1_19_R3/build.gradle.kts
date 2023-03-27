afa7e33be5610a2a37f6f7065e9bb65424dc430e
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-03-26 20:41:14 -0500

    Add 1.19.4 Support

---
+++ nms/1_19_R3/build.gradle.kts
@@ -0,0 +1,41 @@
+import io.github.patrick.gradle.remapper.RemapTask
+
+plugins {
+    id("io.github.patrick.remapper") version "1.4.0"
+}
+
+val mcVersion = "1.19.4"
+val jvmVersion = JavaVersion.VERSION_17
+
+dependencies {
+    api(project(":battlecards-abstract"))
+
+    compileOnly("org.spigotmc:spigot:$mcVersion-R0.1-SNAPSHOT:remapped-mojang")
+}
+
+java {
+    sourceCompatibility = jvmVersion
+    targetCompatibility = jvmVersion
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = jvmVersion.toString()
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = jvmVersion.toString()
+    }
+
+    assemble {
+        dependsOn("remap")
+    }
+
+    remap {
+        dependsOn("shadowJar")
+
+        version.set(mcVersion)
+        action.set(RemapTask.Action.MOJANG_TO_SPIGOT)
+        archiveName.set("${project.name}-${project.version}.jar")
+    }
+}
