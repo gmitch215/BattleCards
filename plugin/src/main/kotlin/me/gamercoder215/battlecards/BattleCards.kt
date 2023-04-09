5f0ef67598569ad7de00fbde1da0bb4b4a88689d
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-04-09 02:46:08 -0500

    Update BattleCards.kt
    Add bStats and UpdateChecker Integrations

--- plugin/src/main/kotlin/me/gamercoder215/battlecards/BattleCards.kt
+++ plugin/src/main/kotlin/me/gamercoder215/battlecards/BattleCards.kt
@@ -1,11 +1,32 @@
 package me.gamercoder215.battlecards
 
+import com.jeff_media.updatechecker.UpdateCheckSource
+import com.jeff_media.updatechecker.UpdateChecker
 import me.gamercoder215.battlecards.api.BattleConfig
+import org.bstats.bukkit.Metrics
 import org.bukkit.plugin.java.JavaPlugin
 
 object BattleCards : JavaPlugin(), BattleConfig {
 
+    const val BSTATS_ID = 18166
+
     override fun onEnable() {
+        // UpdateChecker
+        UpdateChecker(this, UpdateCheckSource.GITHUB_RELEASE_TAG, "GamerCoder215/BattleCards")
+            .setDownloadLink("https://github.com/GamerCoder215/BattleCards/releases/latest/")
+            .setSupportLink("https://discord.gg/WVFNWEvuqX")
+            .setNotifyOpsOnJoin(true)
+            .setChangelogLink("https://github.com/GamerCoder215/BattleCards/releases/latest/")
+            .setUserAgent("Kotlin 1.8.20 BattleCards User Agent")
+            .setColoredConsoleOutput(true)
+            .setDonationLink("https://www.patreon.com/teaminceptus")
+            .setNotifyRequesters(true)
+            .checkEveryXHours(1.0)
+            .checkNow()
+
+        // bStats
+        Metrics(this, BSTATS_ID)
+
         logger.info("Finished!")
     }
 
