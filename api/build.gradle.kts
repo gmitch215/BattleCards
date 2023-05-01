0f9cfbc31ce8d8202768966a13f8e8eb90a786cd
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-04-30 20:28:04 -0500

    Update build.gradle.kts

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,34 +1,31 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     id("org.jetbrains.dokka") version "1.8.10"
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.8-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.13.2-R0.1-SNAPSHOT")
 }
 
 description = "API for the Premium Plugin BattleCards"
 
 tasks {
-
     kotlinSourcesJar {
-        archiveFileName.set("BattleCards-API-${project.version}-sources.jar")
+        archiveClassifier.set("sources")
     }
 
     register("javadocJar", Jar::class.java) {
         dependsOn(dokkaJavadoc)
 
-        archiveFileName.set("BattleCards-API-${project.version}-javadoc.jar")
+        archiveClassifier.set("javadoc")
         from(dokkaJavadoc.flatMap { it.outputDirectory })
     }
 
-    withType<ShadowJar> {
+    shadowJar {
         dependsOn(kotlinSourcesJar, "javadocJar")
-        archiveFileName.set("BattleCards-API-${project.version}.jar")
     }
 }
 
 artifacts {
     add("archives", tasks["javadocJar"])
+    add("archives", tasks.kotlinSourcesJar)
 }
