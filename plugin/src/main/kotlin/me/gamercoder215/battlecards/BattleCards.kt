c1eedbd320d45ebb332ed0f770402f06c42f3600
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-05-05 21:25:08 -0500

    Update BattleCards.kt

--- plugin/src/main/kotlin/me/gamercoder215/battlecards/BattleCards.kt
+++ plugin/src/main/kotlin/me/gamercoder215/battlecards/BattleCards.kt
@@ -1,9 +1,13 @@
 package me.gamercoder215.battlecards
 
+import com.google.common.collect.ImmutableSet
 import com.jeff_media.updatechecker.UpdateCheckSource
 import com.jeff_media.updatechecker.UpdateChecker
 import me.gamercoder215.battlecards.api.BattleConfig
+import me.gamercoder215.battlecards.api.card.BattleCard
 import me.gamercoder215.battlecards.util.CardListener
+import me.gamercoder215.battlecards.wrapper.Wrapper.Companion.w
+import org.apache.commons.lang.mutable.Mutable
 import org.bstats.bukkit.Metrics
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -18,9 +22,9 @@ object BattleCards : JavaPlugin(), BattleConfig {
     override fun onEnable() {
         saveDefaultConfig()
 
+        w.init()
         loadListeners()
-        logger.info("Loaded Listeners...")
-
+        logger.info("Loaded Files...")
 
         // UpdateChecker
         UpdateChecker(this, UpdateCheckSource.GITHUB_RELEASE_TAG, "GamerCoder215/BattleCards")
@@ -28,7 +32,7 @@ object BattleCards : JavaPlugin(), BattleConfig {
             .setSupportLink("https://discord.gg/WVFNWEvuqX")
             .setNotifyOpsOnJoin(true)
             .setChangelogLink("https://github.com/GamerCoder215/BattleCards/releases/latest/")
-            .setUserAgent("Kotlin 1.8.20 BattleCards User Agent")
+            .setUserAgent("GamerCoder/BattleCards v${BattleCards::class.java.`package`.implementationVersion}")
             .setColoredConsoleOutput(true)
             .setDonationLink("https://www.patreon.com/teaminceptus")
             .setNotifyRequesters(true)
@@ -38,6 +42,8 @@ object BattleCards : JavaPlugin(), BattleConfig {
         // bStats
         Metrics(this, BSTATS_ID)
 
+        logger.info("Loaded Addons...")
+
         logger.info("Finished!")
     }
 
@@ -45,8 +51,19 @@ object BattleCards : JavaPlugin(), BattleConfig {
 
     // BattleConfig Implementation
 
+    val cards: MutableSet<Class<out BattleCard<*>>> = mutableSetOf()
+
     override fun getLanguage(): String {
-        return config.getString("language") ?: "en"
+        return config.getString("language", "en")
+    }
+
+    override fun getRegisteredCards(): Set<Class<out BattleCard<*>>> {
+        return ImmutableSet.copyOf(cards)
+    }
+
+    override fun registerCard(card: Class<out BattleCard<*>>) {
+        if (cards.contains(card)) throw IllegalArgumentException("Card ${card.simpleName} already registered")
+        cards.add(card)
     }
 
 }
