c28eed5c3bc39dde0fad9f03ee90c23a22705057
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-06-05 23:39:26 -0500

    Add JvmStatic

--- abstract/src/main/kotlin/me/gamercoder215/battlecards/wrapper/CommandWrapper.kt
+++ abstract/src/main/kotlin/me/gamercoder215/battlecards/wrapper/CommandWrapper.kt
@@ -5,18 +5,22 @@ import com.google.common.collect.ImmutableMap
 interface CommandWrapper {
 
     companion object {
+        @JvmStatic
         val COMMANDS: Map<String, List<String>> = ImmutableMap.builder<String, List<String>>()
             .put("bcard", listOf("card", "battlecard"))
             .build()
 
+        @JvmStatic
         val COMMAND_PERMISSIONS: Map<String, String> = ImmutableMap.builder<String, String>()
             .put("bcard", "battlecards.user.card")
             .build()
 
+        @JvmStatic
         val COMMAND_DESCRIPTIONS: Map<String, String> = ImmutableMap.builder<String, String>()
             .put("bcard", "Main BattleCards Card Command")
             .build()
 
+        @JvmStatic
         val COMMAND_USAGES: Map<String, String> = ImmutableMap.builder<String, String>()
             .put("bcard", "/bcard")
             .build()
