b6108cc238cc1b47d8120e6e9e19f5df7d40ac6e
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-06-06 12:35:53 -0500

    Update CardUtils.kt

--- abstract/src/main/kotlin/me/gamercoder215/battlecards/util/CardUtils.kt
+++ abstract/src/main/kotlin/me/gamercoder215/battlecards/util/CardUtils.kt
@@ -1,15 +1,54 @@
 package me.gamercoder215.battlecards.util
 
+import me.gamercoder215.battlecards.api.BattleConfig
+import me.gamercoder215.battlecards.impl.BlockAttachment
+import me.gamercoder215.battlecards.impl.cards.IBattleCard
 import org.bukkit.ChatColor
+import org.bukkit.Location
+import org.bukkit.entity.ArmorStand
+import org.bukkit.inventory.ItemStack
+import org.bukkit.metadata.FixedMetadataValue
 import org.bukkit.util.ChatPaginator
+import org.bukkit.util.Vector
+import java.text.SimpleDateFormat
 import java.util.*
+import java.util.function.Supplier
 
 object CardUtils {
 
     // Entity Utils
 
-    // String Utils
+    @JvmStatic
+    fun createAttachments(card: IBattleCard<*>) {
+        val attachments = card.javaClass.getAnnotationsByType(BlockAttachment::class.java)
+        val reference = card.getEntity().location
 
+        for (attachment in attachments) {
+            val newLocation = reference.add(getLocal(reference, Vector(attachment.offsetX, attachment.offsetY, attachment.offsetZ)))
+            val entity: ArmorStand = card.getEntity().world.spawn(newLocation, ArmorStand::class.java)
+
+            entity.isSmall = attachment.small
+            entity.isVisible = false
+            entity.setGravity(false)
+            entity.setMetadata("battlecards:block_attachment", FixedMetadataValue(BattleConfig.getPlugin(), true))
+
+            entity.helmet = ItemStack(attachment.material)
+
+            card.attachments[entity.uniqueId] = Supplier{
+                val ref = card.getEntity().location
+                ref.add(getLocal(ref, Vector(attachment.offsetX, attachment.offsetY, attachment.offsetZ)))
+            }
+        }
+    }
+
+    // String Utils / Extensions
+
+    @JvmStatic
+    fun format(string: String, vararg args: Any): String {
+        return String.format(BattleConfig.getConfig().getLocale(), string, *args)
+    }
+
+    @JvmStatic
     fun color(s: String): Array<String> {
        val array: Array<String> = s.trim().split("\\s+".toRegex()).toTypedArray()
        val prefix = if (array[0].startsWith("&")) array[0].substring(1) else ChatColor.GRAY
@@ -27,11 +66,18 @@ object CardUtils {
        )
     }
 
+    @JvmStatic
     fun color(s: Collection<String>): List<String> {
         val list = mutableListOf<String>()
         for (i in s) list.addAll(color(i))
 
         return list
+    }
+
+    @JvmStatic
+    fun dateFormat(date: Date?): String? {
+        if (date == null) return null
+        return SimpleDateFormat("MMM dd, yyyy", BattleConfig.getConfig().getLocale()).format(date)
     }
 
     // Other
@@ -55,9 +101,25 @@ object CardUtils {
         ))
     }
 
+    @JvmStatic
     fun toRoman(number: Long): String {
         val l: Long = ROMAN_NUMERALS.floorKey(number)
         return if (number == l) ROMAN_NUMERALS[number]!! else ROMAN_NUMERALS[l] + toRoman(number - l)
     }
+
+    @JvmStatic
+    fun getLocal(reference: Location, local: Vector): Vector {
+        val base = Vector(0, 0, 1)
+        val left: Vector = base.clone().rotateAroundY(Math.toRadians(-reference.yaw + 90.0))
+        val up: Vector = reference.direction.clone().rotateAroundNonUnitAxis(left, Math.toRadians(-90.0))
+
+        val sway: Vector = left.clone().normalize().multiply(local.x)
+        val heave: Vector = up.clone().normalize().multiply(local.y)
+        val surge: Vector = reference.direction.clone().multiply(local.z)
+
+        return Vector(reference.x, reference.y, reference.z).add(sway).add(heave).add(surge)
+    }
+
+
 
 }
