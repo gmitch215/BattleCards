13feb42caa0497a468eac6b7195c2d1dd0245f45
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-06-07 23:55:32 -0500

    Create BattleMaterial.kt

---
+++ abstract/src/main/kotlin/me/gamercoder215/battlecards/util/BattleMaterial.kt
@@ -0,0 +1,25 @@
+package me.gamercoder215.battlecards.util
+
+import me.gamercoder215.battlecards.wrapper.Wrapper
+import org.bukkit.Material
+import org.bukkit.Material.matchMaterial
+import org.bukkit.inventory.ItemStack
+
+enum class BattleMaterial(
+    private val onLegacy: () -> ItemStack,
+    private val onModern: () -> ItemStack,
+    private val default: Material? = null
+) {
+
+    BLACK_STAINED_GLASS_PANE(
+        { ItemStack(matchMaterial("stained_glass_pane"), 1, 15) },
+        { ItemStack(matchMaterial("black_stained_glass_pane")) }
+    )
+
+    ;
+
+    fun findStack(): ItemStack = if (Wrapper.legacy) onLegacy() else onModern()
+
+    fun find(): Material = findStack().type ?: default ?: Material.AIR
+
+}
