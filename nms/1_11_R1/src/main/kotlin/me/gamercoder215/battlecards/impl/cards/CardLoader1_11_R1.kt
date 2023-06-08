b166c7da6c008a7b5d1a5b891cbbd038b8395b79
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-06-07 23:57:00 -0500

    Update CardLoader1_11_R1.kt
    Load IPrinceHusk

--- nms/1_11_R1/src/main/kotlin/me/gamercoder215/battlecards/impl/cards/CardLoader1_11_R1.kt
+++ nms/1_11_R1/src/main/kotlin/me/gamercoder215/battlecards/impl/cards/CardLoader1_11_R1.kt
@@ -5,7 +5,8 @@ import me.gamercoder215.battlecards.wrapper.CardLoader
 class CardLoader1_11_R1 : CardLoader {
 
     override fun loadedCards(): Collection<Class<out IBattleCard<*>>> = listOf(
-        IMesaZombie::class.java
+        IMesaZombie::class.java,
+        IPrinceHusk::class.java
     )
 
 }
