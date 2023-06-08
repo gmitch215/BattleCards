1de6d7cff56dc5ecdc398ef1808d8cb3b5d76185
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-06-07 23:53:44 -0500

    Add Lamp Dependency, Preserve Parameters

--- nms/1_13_R2/build.gradle.kts
+++ nms/1_13_R2/build.gradle.kts
@@ -1,7 +1,17 @@
+val lampVersion = "3.1.5"
 val mcVersion = "1.13.2"
 
 dependencies {
     api(project(":battlecards-abstract"))
 
+    implementation("com.github.Revxrsal.Lamp:bukkit:$lampVersion")
+    implementation("com.github.Revxrsal.Lamp:common:$lampVersion")
+
     compileOnly("org.spigotmc:spigot:$mcVersion-R0.1-SNAPSHOT")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.javaParameters = true
+    }
 }
