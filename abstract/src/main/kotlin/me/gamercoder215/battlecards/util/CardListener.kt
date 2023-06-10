b97f9e2c1bd9e5717437b48d208a68d38db1bf17
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-06-10 15:49:33 -0500

    Add Interact Events

--- abstract/src/main/kotlin/me/gamercoder215/battlecards/util/CardListener.kt
+++ abstract/src/main/kotlin/me/gamercoder215/battlecards/util/CardListener.kt
@@ -2,18 +2,48 @@ package me.gamercoder215.battlecards.util
 
 import me.gamercoder215.battlecards.impl.Defensive
 import me.gamercoder215.battlecards.impl.Offensive
+import me.gamercoder215.battlecards.util.inventory.Generator
+import me.gamercoder215.battlecards.wrapper.Wrapper.Companion.get
 import me.gamercoder215.battlecards.wrapper.Wrapper.Companion.r
+import me.gamercoder215.battlecards.wrapper.Wrapper.Companion.w
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.event.entity.EntityTargetEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.plugin.Plugin
 
 class CardListener(plugin: Plugin) : Listener {
 
     init {
         plugin.server.pluginManager.registerEvents(this, plugin)
+    }
+
+    @EventHandler
+    fun onInteract(event: PlayerInteractEvent) {
+        if (!event.hasItem()) return
+        val p = event.player
+        val item = event.item!!
+
+        if (!item.isCard()) return
+        val card = item.getCard()!!
+
+        when (event.action) {
+            Action.LEFT_CLICK_AIR, Action.LEFT_CLICK_BLOCK -> {
+                val info = Generator.genGUI(27, get("menu.card_info"))
+                info.isCancelled = true
+
+                p.openInventory(info)
+                p.playSuccess()
+            }
+            Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK -> {
+                card.spawnCard(p)
+                w.spawnParticle(BattleParticle.CLOUD, p.location, 30, 0.0, 1.0, 0.0, 0.2)
+            }
+            else -> return
+        }
     }
 
     @EventHandler
