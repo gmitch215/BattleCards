ca101b143db627798544fb8947bf03ef3722a2c6
Gregory Mitchell <gamercoder215@teaminceptus.us>
2023-07-09 08:33:14 -0400

    Add Basic Card Modifiers

--- abstract/src/main/kotlin/me/gamercoder215/battlecards/impl/cards/IBasicCard.kt
+++ abstract/src/main/kotlin/me/gamercoder215/battlecards/impl/cards/IBasicCard.kt
@@ -1,9 +1,11 @@
 package me.gamercoder215.battlecards.impl.cards
 
 import me.gamercoder215.battlecards.api.card.BattleCardType
-import me.gamercoder215.battlecards.impl.ICard
-import me.gamercoder215.battlecards.impl.Type
+import me.gamercoder215.battlecards.impl.*
 import org.bukkit.entity.Creature
 
 @Type(BattleCardType.BASIC)
+@AttributesModifier(CardAttribute.MAX_HEALTH, CardOperation.ADD, 5.0)
+@AttributesModifier(CardAttribute.DEFENSE, CardOperation.ADD, 1.0)
+@AttributesModifier(CardAttribute.ATTACK_DAMAGE, CardOperation.ADD, 0.5)
 class IBasicCard<T : Creature>(data: ICard) : IBattleCard<T>(data)
