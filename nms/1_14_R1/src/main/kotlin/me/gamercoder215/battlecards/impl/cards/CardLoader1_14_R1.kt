84d3474801ea34bb140bf412a0e457256941777d
Gregory Mitchell <gamercoder215@teaminceptus.us>
2023-07-09 08:36:10 -0400

    Create CardLoader1_14_R1.kt

---
+++ nms/1_14_R1/src/main/kotlin/me/gamercoder215/battlecards/impl/cards/CardLoader1_14_R1.kt
@@ -0,0 +1,15 @@
+package me.gamercoder215.battlecards.impl.cards
+
+import me.gamercoder215.battlecards.wrapper.CardLoader
+
+internal class CardLoader1_14_R1 : CardLoader {
+
+    override fun loadedCards(): Collection<Class<out IBattleCard<*>>> = listOf(
+        IBomberman::class.java,
+        IGoldenWizard::class.java,
+        IMercenary::class.java,
+        IPhantomRider::class.java,
+        IRaider::class.java
+    )
+
+}
