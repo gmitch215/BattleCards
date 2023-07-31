bbdc303fdeec7e387ab691196b7de02dad0c6f76
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-07-31 00:07:18 -0500

    Update IThunderRevenant.kt
    Fix Lightning Damage Loop

--- abstract/src/main/kotlin/me/gamercoder215/battlecards/impl/cards/IThunderRevenant.kt
+++ abstract/src/main/kotlin/me/gamercoder215/battlecards/impl/cards/IThunderRevenant.kt
@@ -2,9 +2,13 @@ package me.gamercoder215.battlecards.impl.cards
 
 import me.gamercoder215.battlecards.api.card.BattleCardType
 import me.gamercoder215.battlecards.impl.*
+import me.gamercoder215.battlecards.util.isCard
+import org.bukkit.ChatColor
 import org.bukkit.entity.LivingEntity
 import org.bukkit.entity.Zombie
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
 
@@ -13,9 +17,17 @@ import org.bukkit.potion.PotionEffectType
 @AttributesModifier(CardAttribute.MAX_HEALTH, CardOperation.ADD, 3.25)
 @AttributesModifier(CardAttribute.ATTACK_DAMAGE, CardOperation.ADD, 0.32)
 @AttributesModifier(CardAttribute.DEFENSE, CardOperation.ADD, 4.55)
+@Rideable
 class IThunderRevenant(data: ICard) : IBattleCard<Zombie>(data) {
+    
+    @CardAbility("card.thunder_revenant.ability.heat_resistance", ChatColor.RED)
+    @Damage
+    private fun heatResistance(event: EntityDamageEvent) {
+        if (event.cause == DamageCause.FIRE || event.cause == DamageCause.FIRE_TICK || event.cause == DamageCause.LIGHTNING)
+            event.isCancelled = true
+    }
 
-    @CardAbility("card.thunder_revenant.ability.electricity")
+    @CardAbility("card.thunder_revenant.ability.electricity", ChatColor.YELLOW)
     @Offensive(0.2, CardOperation.ADD, 0.05, 0.75)
     private fun electricity(event: EntityDamageByEntityEvent) {
         val target = event.entity as? LivingEntity ?: return
@@ -31,11 +43,11 @@ class IThunderRevenant(data: ICard) : IBattleCard<Zombie>(data) {
         val target = event.entity as? LivingEntity ?: return
 
         target.world.strikeLightning(target.location)
-        target.damage(statistics.attackDamage * 1.15, entity)
+        target.damage(if (event.damager.isCard) statistics.attackDamage * 1.15 else r.nextDouble(5.0, 20.0))
         target.fireTicks += 20 * 4
     }
 
-    @CardAbility("card.thunder_revenant.ability.paralysis")
+    @CardAbility("card.thunder_revenant.ability.paralysis", ChatColor.GOLD)
     @Offensive(0.4, CardOperation.ADD, 0.05)
     @UnlockedAt(15)
     private fun paralysis(event: EntityDamageByEntityEvent) {
