ab59403d78c37b8d94579fad17d854c7f7afc8c5
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-07-31 20:08:02 -0500

    Add Card Tagline and Description to Card Info

--- abstract/src/main/kotlin/me/gamercoder215/battlecards/util/inventory/Generator.kt
+++ abstract/src/main/kotlin/me/gamercoder215/battlecards/util/inventory/Generator.kt
@@ -1,22 +1,32 @@
 package me.gamercoder215.battlecards.util.inventory
 
 import me.gamercoder215.battlecards.api.card.Card
+import me.gamercoder215.battlecards.api.card.Rarity
 import me.gamercoder215.battlecards.util.BattleMaterial
+import me.gamercoder215.battlecards.util.CardUtils
 import me.gamercoder215.battlecards.wrapper.BattleInventory
 import me.gamercoder215.battlecards.wrapper.Wrapper.Companion.get
 import me.gamercoder215.battlecards.wrapper.Wrapper.Companion.w
 import org.bukkit.ChatColor
+import org.bukkit.Material
+import org.bukkit.entity.Creature
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.LivingEntity
+import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.SkullMeta
+import org.bukkit.util.ChatPaginator
 import kotlin.math.floor
 
 
 object Generator {
 
+    @JvmStatic
     fun genGUI(size: Int, name: String?): BattleInventory {
         return genGUI("", size, name)
     }
 
+    @JvmStatic
     fun genGUI(key: String, size: Int, name: String?): BattleInventory {
         if (size < 9 || size > 54) throw IllegalArgumentException("Size must be between 9 and 54")
         if (size % 9 > 0) throw IllegalArgumentException("Size must be a multiple of 9")
@@ -55,9 +65,57 @@ object Generator {
     }
 
     @JvmStatic
+    private val typeToItem: Map<EntityType, ItemStack> =
+        mapOf<Any, Any>(
+            EntityType.BLAZE to "MHF_Blaze",
+            EntityType.CAVE_SPIDER to "MHF_CaveSpider",
+            EntityType.CREEPER to "MHF_Creeper",
+            EntityType.ENDERMAN to "MHF_Enderman",
+            EntityType.IRON_GOLEM to "MHF_Golem",
+            EntityType.SKELETON to "MHF_Skeleton",
+            EntityType.SPIDER to "MHF_Spider",
+            EntityType.WITHER to Material.NETHER_STAR,
+
+            "wither_skeleton" to "MHF_WSkeleton",
+        ).mapNotNull {
+            val type: EntityType = when (val key = it.key) {
+                is EntityType -> key
+                is String -> try { EntityType.valueOf(key.uppercase()) } catch (e: IllegalArgumentException) { null }
+                else -> throw IllegalArgumentException("Invalid Type ${it.key::class.simpleName}")
+            } ?: return@mapNotNull null
+
+            val item: ItemStack = when (val value = it.value) {
+                is Material -> ItemStack(value)
+                is ItemStack -> value
+                is String -> BattleMaterial.PLAYER_HEAD.findStack().apply {
+                    itemMeta = (itemMeta as SkullMeta).apply { owner = value }
+                }
+                else -> throw IllegalArgumentException("Invalid Type ${it.value::class.simpleName}")
+            }
+
+            type to item
+        }.toMap().toMutableMap().apply {
+            for (type in EntityType.entries.filter { Creature::class.java.isAssignableFrom(it.entityClass ?: LivingEntity::class.java) })
+                if (!containsKey(type)) this[type] = ItemStack(Material.matchMaterial("${type.name}_SPAWN_EGG") ?: BattleMaterial.FILLED_MAP.find())
+        }
+
+    @JvmStatic
     fun generateCardInfo(card: Card): BattleInventory {
         val inv = genGUI(27, get("menu.card.info"))
         inv.isCancelled = true
+
+        inv[4] = (if (card.type.icon == null) typeToItem[card.entityCardType] ?: BattleMaterial.FILLED_MAP.findStack() else ItemStack(card.type.icon)).apply {
+            itemMeta = itemMeta.apply {
+                displayName = CardUtils.format(get("constants.card"), "${card.rarity.color}${card.name}")
+
+                if (card.rarity != Rarity.BASIC)
+                    lore = ChatPaginator.wordWrap("\"${get("card.${card.type.name.lowercase()}")}\"", 30).map { s -> "${ChatColor.YELLOW}$s" } +
+                            arrayOf(" ") +
+                            ChatPaginator.wordWrap(get("card.${card.type.name.lowercase()}.desc"), 30).map { s -> "${ChatColor.GRAY}$s" }
+
+                addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ENCHANTS, ItemFlag.HIDE_POTION_EFFECTS)
+            }
+        }
 
         val info = CardGenerator.generateCardInfo(card)
         val stats = CardGenerator.generateCardStatistics(card)
