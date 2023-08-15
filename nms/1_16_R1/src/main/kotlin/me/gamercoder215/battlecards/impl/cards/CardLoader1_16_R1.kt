850f63f01a05dea9d06b03e3b2b2a756eea9cf58
Gregory Mitchell <54124162+GamerCoder215@users.noreply.github.com>
2023-08-15 18:25:58 -0500

    Update CardLoader1_16_R1.kt

--- nms/1_16_R1/src/main/kotlin/me/gamercoder215/battlecards/impl/cards/CardLoader1_16_R1.kt
+++ nms/1_16_R1/src/main/kotlin/me/gamercoder215/battlecards/impl/cards/CardLoader1_16_R1.kt
@@ -1,17 +1,12 @@
 package me.gamercoder215.battlecards.impl.cards
 
 import me.gamercoder215.battlecards.util.inventory.Items
-import me.gamercoder215.battlecards.util.inventory.Items.randomCumulative
+import me.gamercoder215.battlecards.util.inventory.Items.random
 import me.gamercoder215.battlecards.wrapper.CardLoader
-import me.gamercoder215.battlecards.wrapper.Wrapper
 import me.gamercoder215.battlecards.wrapper.Wrapper.Companion.r
-import org.bukkit.Material
-import org.bukkit.block.Chest
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
-import org.bukkit.event.world.ChunkPopulateEvent
 import org.bukkit.event.world.LootGenerateEvent
-import kotlin.math.roundToInt
 
 internal class CardLoader1_16_R1 : CardLoader, Listener {
 
@@ -31,7 +26,7 @@ internal class CardLoader1_16_R1 : CardLoader, Listener {
         val limit = (luck / 0.5F).toInt().coerceAtMost(3)
 
         for (i in 0 until limit) {
-            val item = Items.EFFECTIVE_GENERATED_ITEMS.randomCumulative(luck.toInt().coerceAtMost(10))
+            val item = Items.EFFECTIVE_GENERATED_ITEMS.random(luck.toInt().coerceAtMost(10)) ?: continue
             event.loot.add(item)
         }
     }
